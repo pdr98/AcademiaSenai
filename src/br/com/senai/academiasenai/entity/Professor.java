7b50a9c3f1f591642c3a559b8dd8e9d4fc1cc253
pdr98 <pedrosnider98@gmail.com>
2022-03-17 21:46:03 -0300

    Adicionado novo atributo

--- src/br/com/senai/academiasenai/entity/Professor.java
+++ src/br/com/senai/academiasenai/entity/Professor.java
@@ -1,9 +1,12 @@
 package br.com.senai.academiasenai.entity;
 
+import java.time.LocalDate;
+
 public class Professor {
 
     private Integer id;
     private String nomeCompleto;
+    private LocalDate dataDeAdmissao;
 
     public Integer getId() {
         return id;
@@ -19,5 +22,13 @@ public class Professor {
 
     public void setNomeCompleto(String nomeCompleto) {
         this.nomeCompleto = nomeCompleto;
+    }
+
+    public LocalDate getDataDeAdmissao() {
+        return dataDeAdmissao;
+    }
+
+    public void setDataDeAdmissao(LocalDate dataDeAdmissao) {
+        this.dataDeAdmissao = dataDeAdmissao;
     }
 }
