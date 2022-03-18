0ab90a530790923d2f2669c7a931a9febc41ad79
pdr98 <pedrosnider98@gmail.com>
2022-03-17 22:07:46 -0300

    Resolucao de conflitos

--- src/br/com/senai/academiasenai/entity/Professor.java
+++ src/br/com/senai/academiasenai/entity/Professor.java
@@ -1,12 +1,9 @@
 package br.com.senai.academiasenai.entity;
 
-import java.time.LocalDate;
-
 public class Professor {
 
     private Integer id;
     private String nomeCompleto;
-    private LocalDate dataDeCasamento;
 
     public Integer getId() {
         return id;
@@ -24,11 +21,4 @@ public class Professor {
         this.nomeCompleto = nomeCompleto;
     }
 
-    public LocalDate getDataDeCasamento() {
-        return dataDeCasamento;
-    }
-
-    public void setDataDeCasamento(LocalDate dataDeCasamento) {
-        this.dataDeCasamento = dataDeCasamento;
-    }
 }
